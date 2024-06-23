6d37713947a1e7772a547424f47a6551e588cd87
Bruce Wang <brucewang.korea@gmail.com>
2024-06-23 15:44:55 -0400

    separate the index script and changed the JS injecting point
    So that the image map works fine with Firefox

--- script/index.js
+++ script/index.js
@@ -17,34 +17,6 @@ function randomizeProfile() {
 // Referenced from...
 // https://codepen.io/jmikelf/pen/eWKNZd
 $(document).ready(function () {
-    // set the image-map width and height to match the img size
-    $('#image-map').css({
-        'width': $('#image-map img').width(),
-        'height': $('#image-map img').height()
-    })
-    //tooltip direction
-    var tooltipDirection;
-    for (i = 0; i < $(".pin").length; i++) {
-        // set tooltip direction type - up or down             
-        if ($(".pin").eq(i).hasClass('pin-down')) {
-            tooltipDirection = 'tooltip-down';
-        } else {
-            tooltipDirection = 'tooltip-up';
-        }
-
-        // append the tooltip
-        $("#image-map").append("<div style='left:" + $(".pin").eq(i).data('xpos') + "px;top:" + $(".pin").eq(i).data('ypos') + "px' class='" + tooltipDirection + "'>\
-                                            <div class='uolteam26tooltip'>" + $(".pin").eq(i).html() + "</div>\
-                                    </div>");
-    }
-
-    // show/hide the tooltip
-    $('.tooltip-up, .tooltip-down').mouseenter(function () {
-        $(this).children('.uolteam26tooltip').fadeIn(100);
-    }).mouseleave(function () {
-        $(this).children('.uolteam26tooltip').fadeOut(100);
-    })
-
     randomizeProfile();
 });
 
